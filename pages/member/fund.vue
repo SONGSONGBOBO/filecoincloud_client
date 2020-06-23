0867dd35a2059ce349555eec51b66aa3d6ca5cb1
wang2hao <378619897@qq.com>
2020-06-23 10:47:17 +0800

    我的地址页面 ui修改

--- pages/member/fund.vue
+++ pages/member/fund.vue
@@ -1,27 +1,78 @@
 <template>
-	<view class="whiteBg">
-		<view class="tabContent">
+	<view class="whiteBg fund-container">
+		<!-- <view class="tabContent">
 			<view class="tab">
 				<view class="tabItem"  v-for="(item, index) in coins" :class="'{activeTabItem:current=='+index+'}'" @click="tab(index)" v-text="item"></view>
-				
-				<!-- <view class="tabItem" :class="{activeTabItem:current==1}" @click="tab(1)">每日币读</view> -->
 			</view>
 		</view>
-		
-		
 		<view class="member-menu">
-			
 			<navigator url="/pages/order/order">
 				<view class="menu-item">
 					
 					<view>filecoin地址</view>
 					<view>xxxxx</view>
 				</view> 
-			</navigator>
+			</navigator> 
+		</view> -->
+		
+		<!-- 新增内容 -->
+		<!-- 导航 -->
+		<view class="nav-x">
+			<!-- 每一项导航 -->
+			<view class="nav-item">
+				<image class="item-img" src="../../static/image/u12.png" mode="aspectFill"></image>
+				<text class="item-text">BTC</text>
+			</view>
+			<!-- 每一项导航 end -->
+			<!-- 每一项导航 -->
+			<view class="nav-item item-active">
+				<image class="item-img" src="../../static/image/u12.png" mode="aspectFill"></image>
+				<text class="item-text">BTC</text>
+			</view>
+			<!-- 每一项导航 end -->
+			<!-- 每一项导航 -->
+			<view class="nav-item">
+				<image class="item-img" src="../../static/image/u12.png" mode="aspectFill"></image>
+				<text class="item-text">BTC</text>
+			</view>
+			<!-- 每一项导航 end -->
+			<!-- 每一项导航 -->
+			<view class="nav-item">
+				<image class="item-img" src="../../static/image/u12.png" mode="aspectFill"></image>
+				<text class="item-text">BTC</text>
+			</view>
+			<!-- 每一项导航 end -->
+			<!-- 每一项导航 -->
+			<view class="nav-item">
+				<image class="item-img" src="../../static/image/u12.png" mode="aspectFill"></image>
+				<text class="item-text">BTC</text>
+			</view>
+			<!-- 每一项导航 end -->
+		</view>
+		<!-- 导航 end -->
+		
+		<!-- 分割线 -->
+		<view class="diving"></view>
 			
-			
+		<!-- 地址 -->
+		<view class="address-container">
+			<!-- 每一条地址 -->
+			<view v-for="(item ,index) in 6" :key="index" class="address-row f-col">
+				<view class="f-row f-jf-between f-al-center">
+					<text class="text-2x">BTC地址</text>
+					<view>
+						<text class="text-2x text-gray">操作</text>
+						<text class="cuIcon-right text-gray pd-l-10px"></text>
+					</view>
+				</view>
+				<!-- 地址 -->
+				<text class="text-2x text-gray">YIUHsadhiashdiauh9812312awsda</text>
+			</view>
+			<!-- 每一条地址 end -->
 		</view>
 		
+		
+		<!-- 新增内容 end -->
 		
 		<view style="height: 80px;"></view>
 	</view>
@@ -127,7 +178,89 @@
 	}
 </script>
 
-<style> 
+<style lang="less"> 
+
+// 新增样式
+@pagePD: 25rpx;
+	@Color1: rgb(40, 208, 148);
+	@Color2: #EE7A39;
+	.color1{ color: @Color1; }
+	.color2{ color: @Color2; }
+	.pd-1x{ 
+		padding-right: @pagePD;
+		padding-left: @pagePD;
+	}
+	.pd-2x{
+		padding-right: calc(@pagePD *2);
+		padding-left: calc(@pagePD *2);
+	}
+	.pd-1y{
+		padding-top: @pagePD;
+		padding-bottom: @pagePD;
+	}
+	.pd-2y{
+		padding-top: calc(@pagePD *2);
+		padding-bottom: calc(@pagePD *2);
+	}
+.fund-container{
+	*{box-sizing: border-box;}
+	// 导航
+	.nav-x{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding: 15rpx @pagePD;
+		
+		width: 750rpx;
+		overflow-x: scroll;
+		.nav-item{
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			padding: 20rpx @pagePD;
+			.item-img{
+				width: 50rpx;
+				height: 50rpx;
+			}
+			.item-text{
+				color: #333333;
+				padding-left: 10rpx;
+				font-size: 40rpx;
+			}
+		}
+		.item-active{
+			.item-text{
+				color: #FFFFFF;
+			}
+			border-radius: 2000px;
+			background:linear-gradient(325deg,rgba(70,182,255,1) 0%,rgba(114,145,255,1) 100%);
+		}
+	}
+	
+	//分割线
+	.diving{
+		width: 750rpx;
+		height: 25rpx;
+		border-top: solid 1px #dddddd;
+		border-bottom: solid 1px #dddddd;
+		background-color: #f4f4f4;
+		margin: 30rpx 0;
+	}
+	
+	//地址
+	.address-container{
+		width: 750rpx;
+		padding: 0 @pagePD;
+		display: flex;
+		flex-direction: column;
+		.address-row{
+			padding: 15rpx 15rpx;
+			border-bottom: 1px solid #f4f4f4;
+		}
+	}
+	
+}
+// 新增样式 end
 
 .tabContent image{width: 100%;display: block;}
 .tab{height: 80rpx;display: flex;width: 100%;align-items: center;
